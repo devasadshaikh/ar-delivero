e1385550a1dde7649fc1e23870e1cda126afab8c
devasadshaikh <officialshaikhasad@gmail.com>
2024-06-15 03:35:34 +0400

    commit1

--- src/ui-components/faqs.tsx
+++ src/ui-components/faqs.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const FAQS = () => {
   return (
     <>
-    <h1 className=' ml-14 font-bold text-3xl mt-72'>Order food and groceries online from the best restaurants and shops on foodpanda</h1>
-    <p className=' mt-5 text-balance ml-14'>Are you hungry? Did you have a long and stressful day? Interested in getting a cheesy pizza delivered to your office or looking to avoid the weekly shop? Then foodpanda Pakistan is the right destination for you! foodpanda offers you a long and detailed list of the best restaurants and shops near you to help make your every day easier.</p>
+    <h1 className=' ml-14 font-bold text-3xl mt-72'>`Order food and groceries online from the best restaurants and shops on foodpanda`</h1>
+    <p className=' mt-5 text-balance ml-14'>`Are you hungry? Did you have a long and stressful day? Interested in getting a cheesy pizza delivered to your office or looking to avoid the weekly shop? Then foodpanda Pakistan is the right destination for you! foodpanda offers you a long and detailed list of the best restaurants and shops near you to help make your every day easier.`</p>
     
-    <h1 className=' ml-14 font-bold text-3xl mt-10'>What's new?</h1>
-    <p className=' mt-5 text-balance ml-14'>✓ Wide variety of restaurants and shops, an abundance of cuisines & products.</p>
-    <p className=' mt-3 text-balance ml-14'>✓ High quality delivery service.</p>
-    <p className=' mt-3 text-balance ml-14'>✓ Live chat feature to give App users instant help when they need it.</p>
-    <p className=' mt-3 text-balance ml-14'>✓ NEW: foodpanda grocery delivery! Discover the best shops, pharmacies, bakeries and more near you.</p>
+    <h1 className=' ml-14 font-bold text-3xl mt-10'>`What's new?`</h1>
+    <p className=' mt-5 text-balance ml-14'>`✓ Wide variety of restaurants and shops, an abundance of cuisines & products.`</p>
+    <p className=' mt-3 text-balance ml-14'>`✓ High quality delivery service.`</p>
+    <p className=' mt-3 text-balance ml-14'>`✓ Live chat feature to give App users instant help when they need it.`</p>
+    <p className=' mt-3 text-balance ml-14'>`✓ NEW: foodpanda grocery delivery! Discover the best shops, pharmacies, bakeries and more near you.`</p>
     </>
   )
 }
--- src/ui-components/smallCard.tsx
+++ src/ui-components/smallCard.tsx
@@ -5,10 +5,10 @@ const SmallCard = () => {
   return (
     <>
      <div className=' w-5/12 h-96 bg-white rounded-xl shadow-md shadow-slate-300 z-50  absolute ml-24 mt-[42rem]'>
-     <h1 className=' font-semibold text-2xl ml-5 mt-3'>List your restaurant or shop on foodpanda</h1>
-     <p className=' font-medium text-lg text-balance ml-5 mt-6'>Would you like millions of new customers to enjoy your amazing food and groceries? So would we!</p>
-     <p className=' font-medium text-lg  ml-5 mt-6'>It's simple: we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat!</p>
-     <p className=' font-medium text-lg text-balance ml-5 mt-6'>Interested? Let's start our partnership today!</p>
+     <h1 className=' font-semibold text-2xl ml-5 mt-3'>`List your restaurant or shop on foodpanda`</h1>
+     <p className=' font-medium text-lg text-balance ml-5 mt-6'>`Would you like millions of new customers to enjoy your amazing food and groceries? So would we!`</p>
+     <p className=' font-medium text-lg  ml-5 mt-6'>`It's simple: we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat!`</p>
+     <p className=' font-medium text-lg text-balance ml-5 mt-6'>`Interested? Let's start our partnership today!`</p>
 
      <Button className=' ml-5 mt-6'>Get Started</Button>
 
